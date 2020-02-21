d299e891bc3f444b9e3e37ceebdb12c903272f0e
DevExpressExampleBot <fakeEmail>
2020-02-21 15:07:12 +0300

    Source auto update [skip ci]

--- VB/HowToGroupFields/MainWindow.xaml.vb
+++ VB/HowToGroupFields/MainWindow.xaml.vb
@@ -1,22 +1,24 @@
 ﻿Imports System.Windows
-Imports DataSet1
-Imports DataSet1TableAdapters
 Imports DevExpress.Xpf.PivotGrid
+Imports HowToBindToMDB.DataSet1TableAdapters
+Imports HowToBindToMDB.DataSet1
 
 Namespace HowToBindToMDB
-    Partial Public Class MainWindow
-        Inherits Window
-        Private salesPersonDataTable As New SalesPersonDataTable()
-        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
-        Public Sub New()
-            InitializeComponent()
-            pivotGridControl1.DataSource = salesPersonDataTable
-        End Sub
-        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            salesPersonDataAdapter.Fill(salesPersonDataTable)
+	Partial Public Class MainWindow
+		Inherits Window
 
-            ' Create a group at run-time
-            Dim group As PivotGridGroup = pivotGridControl1.Groups.Add(fieldCategoryName, fieldProductName)
-        End Sub
-    End Class
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New HowToBindToMDB.DataSet1.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
+		End Sub
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
+
+			' Create a group at run-time
+			Dim group As PivotGridGroup = pivotGridControl1.Groups.Add(fieldCategoryName, fieldProductName)
+		End Sub
+	End Class
 End Namespace
